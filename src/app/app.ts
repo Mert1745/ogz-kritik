9622a5e35d1b5e6f9a08b7ebdb614e0b002557f3
mert1745 <mertkose1745@gmail.com>
2025-12-09 23:09:11 +0300

    Add PrimeNG

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { CommonModule } from '@angular/common';
+import { TooltipModule } from 'primeng/tooltip';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ButtonModule, CardModule, CommonModule, TooltipModule, RippleModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('ogz-kritik');
+  protected readonly isDarkMode = signal(false);
+
+  toggleDarkMode() {
+    this.isDarkMode.set(!this.isDarkMode());
+  }
 }
