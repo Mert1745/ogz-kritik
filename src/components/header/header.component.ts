6ac9d0beed8c3b632e12aa02ccde52c4c0998219
mert1745 <mertkose1745@gmail.com>
2025-12-27 19:58:36 +0300

    Stylize review page a little

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,11 +1,12 @@
-import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {RouterLink} from '@angular/router';
 import {ButtonModule} from 'primeng/button';
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [CommonModule, ButtonModule, NgOptimizedImage],
+    imports: [CommonModule, ButtonModule, NgOptimizedImage, RouterLink],
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 })
--- src/pages/review/review.component.ts
+++ src/pages/review/review.component.ts
@@ -1,23 +1,37 @@
-import {Component, computed, Signal} from '@angular/core';
+import {Component, computed, signal, Signal} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DetailedIndexService} from '../../services/detailed-index.service';
 import {DetailedIndex} from '../../interface';
+import {PaginatorModule, PaginatorState} from 'primeng/paginator';
+import {CardModule} from 'primeng/card';
 
 @Component({
     selector: 'app-review',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, PaginatorModule, CardModule],
     templateUrl: './review.component.html',
     styleUrls: ['./review.component.css']
 })
 export class ReviewComponent {
     itemsReview: Signal<DetailedIndex[]>;
+    first = signal(0);
+    rows = 50;
+
+    paginatedItems: Signal<DetailedIndex[]>;
 
     constructor(private detailedIndexService: DetailedIndexService) {
         this.itemsReview = computed(() =>
             this.detailedIndexService.detailedIndex()
                 .filter(item => item.section === 'İnceleme')
         );
+
+        this.paginatedItems = computed(() =>
+            this.itemsReview().slice(this.first(), this.first() + this.rows)
+        );
+    }
+
+    onPageChange(event: PaginatorState) {
+        this.first.set(event.first ?? 0);
     }
 }
 
