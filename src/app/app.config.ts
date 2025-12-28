61adff877d02d5c8aae3e6a1838fe471d5ea1f64
mert1745 <mertkose1745@gmail.com>
2025-12-28 13:48:56 +0300

    Change primary color

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,17 +12,17 @@ import { provideClientHydration, withEventReplay, withIncrementalHydration } fro
 const MyPreset = definePreset(Aura, {
   semantic: {
     primary: {
-      50: '{red.50}',
-      100: '{red.100}',
-      200: '{red.200}',
-      300: '{red.300}',
-      400: '{red.400}',
-      500: '{red.500}',
-      600: '{red.600}',
-      700: '{red.700}',
-      800: '{red.800}',
-      900: '{red.900}',
-      950: '{red.950}'
+      50: '#f6f7f6',
+      100: '#e3e7e4',
+      200: '#c7cfc9',
+      300: '#9ba89f',
+      400: '#7d8e84',
+      500: '#62746a',
+      600: '#4d5d54',
+      700: '#404c45',
+      800: '#363f38',
+      900: '#2e352f',
+      950: '#1a1e1b'
     },
     colorScheme: {
       light: {
