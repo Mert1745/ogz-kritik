0ceebc71443c613ea39ce4b722ba5ef8d35cc82f
mertk <mertkose1745@gmail.com>
2026-01-14 23:53:04 +0300

    Add a 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { DashboardComponent } from '../components/dashboard/dashboard.component'
 import { ReviewComponent } from '../pages/review/review.component';
 import { MagazineComponent } from '../pages/magazine/magazine.component';
 import { AuthorComponent } from '../pages/author/author.component';
+import { NotFoundComponent } from '../pages/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: '', component: DashboardComponent },
     { path: 'review', component: ReviewComponent },
     { path: 'magazine', component: MagazineComponent },
     { path: 'author', component: AuthorComponent },
+    { path: '**', component: NotFoundComponent },
 ];
