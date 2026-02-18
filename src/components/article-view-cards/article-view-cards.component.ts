40798b60dc16b4b2a8959b507ae9e683b549f95e
mertk <mertkose1745@gmail.com>
2026-02-18 17:48:09 +0300

    Add images to magazine page as well

--- src/components/article-view-cards/article-view-cards.component.ts
+++ src/components/article-view-cards/article-view-cards.component.ts
@@ -1,13 +1,15 @@
 import {Component, Input} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 import {CardModule} from 'primeng/card';
 import {DetailedIndex} from '../../interface';
 import {formatMonths} from '../../util/index-mapper';
+import {GameMappingService} from '../../services/game-mapping.service';
+import {REVIEW} from '../../constants';
 
 @Component({
     selector: 'app-article-view-cards',
     standalone: true,
-    imports: [CommonModule, CardModule],
+    imports: [CommonModule, CardModule, NgOptimizedImage],
     templateUrl: './article-view-cards.component.html',
     styleUrls: ['./article-view-cards.component.css']
 })
@@ -16,14 +18,18 @@ export class ArticleViewCardsComponent {
     @Input() onCardClick: (id: number) => void = () => {};
     @Input() dividerOn!: boolean;
 
-    hasScore(item: DetailedIndex): boolean {
-        return !!(item.scoreIn100 || item.scoreIn10 || item.scoreIn5);
+    constructor(private gameMappingService: GameMappingService) {
     }
 
-    getDisplayScore(item: DetailedIndex): number | null {
-        if (item.scoreIn100) return item.scoreIn100;
-        if (item.scoreIn10) return item.scoreIn10;
-        if (item.scoreIn5) return item.scoreIn5;
+    hasScore(item: DetailedIndex): boolean {
+        return item.section === REVIEW || !!(item.scoreIn100 || item.scoreIn10 || item.scoreIn5);
+    }
+
+    getDisplayScore(item: DetailedIndex): string | null {
+        if (item.scoreIn100) return item.scoreIn100.toString();
+        if (item.scoreIn10) return item.scoreIn10.toString();
+        if (item.scoreIn5) return item.scoreIn5.toString();
+        if (item.section === REVIEW) return "-";
         return null;
     }
 
@@ -50,6 +56,18 @@ export class ArticleViewCardsComponent {
         } else {
             return 'score-poor';
         }
+    }
+
+    getAppIdByTitle(title: string): number | null {
+        const gameMap = this.gameMappingService.gameMapping();
+        for (const [appid, gameNames] of gameMap.entries()) {
+            if (gameNames.some(gameName =>
+                gameName.toLocaleLowerCase('en-US').trim() === title.toLocaleLowerCase('en-US').trim()
+            )) {
+                return appid;
+            }
+        }
+        return null;
     }
 
     protected readonly formatMonths = formatMonths;
